f0d437d08d8ffb737940f72139681c107882f191
abegorov <abegorov@github.com>
2025-02-22 16:29:09 +0300

    Показывать внутренние IP адреса.

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,11 @@
-output "instance_ips" {
+output "internal_ips" {
+  description = "Private IPs of the instances."
+  value = merge({
+    for h in yandex_compute_instance.default :
+    h.hostname => h.network_interface.0.ip_address
+  })
+}
+output "external_ips" {
   description = "Public IPs of the instances."
   value = merge({
     for h in yandex_compute_instance.default :
